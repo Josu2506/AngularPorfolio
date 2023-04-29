8b8bc999c1afdf56426ca8c9f594128e93766513
Josu Adan <adan.josu92@gmail.com>
2023-04-29 13:40:37 +0200

    Handling error

--- angular-portfolio/src/app/services/work-list.service.ts
+++ angular-portfolio/src/app/services/work-list.service.ts
@@ -1,21 +1,42 @@
 import { Injectable } from '@angular/core';
-import { WORKS } from 'src/assets/files/mock-works';
-import { WorkModel } from '../models/work.model';
-import { Observable, of } from 'rxjs';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable, of } from 'rxjs';
+import { catchError, map, tap } from 'rxjs/operators';
+import { WorkModel } from '../models/work.model';
+// import { WORKS } from 'src/assets/files/mock-works';
 
 @Injectable({
   providedIn: 'root'
 })
 export class WorkListService {
 
-  works: WorkModel[] = WORKS;
+  // works: WorkModel[] = WORKS;
   private worksUrl = 'api/works'
 
   constructor(private http: HttpClient) { }
+/**
+ * Handle Http operation that failed.
+ * Let the app continue.
+ *
+ * @param operation - name of the operation that failed
+ * @param result - optional value to return as the observable result
+ */
+  private handleError<T>(operation = 'operation', result?: T) {
+    return (error: any): Observable<T> => {
 
+          // TODO: send the error to remote logging infrastructure
+    console.error(error); // log to console instead
+
+    // TODO: better job of transforming error for user consumption
+    console.log(`${operation} failed: ${error.status}`);
+
+    return of(result as T);
+  };
+}
 
   getWorks():Observable<WorkModel[]> {
-    return this.http.get<WorkModel[]>(this.worksUrl);
+    return this.http.get<WorkModel[]>(this.worksUrl).pipe(
+      catchError(this.handleError<WorkModel[]>('getWorks', []))
+    );
   }
 }
